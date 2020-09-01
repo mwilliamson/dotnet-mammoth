67b53bb8a517c65fa560047fc2adaf33fe364f88
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2020-08-31 20:40:15 -0400

    Downgraded to .Net Standard 2.0 for wider compatibility. Migrated Mammoth.Cli.

--- Mammoth.Cli/Program.cs
+++ Mammoth.Cli/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using Mammoth;
 
-namespace Mammoth.Cli {
-	internal class Program {
+namespace Mammoth.Cli
+{
+    internal class Program {
 		public static void Main(string[] args) {
 			var result = new DocumentConverter().ConvertToHtml(args[0]);
 			Console.WriteLine(result.Value);
